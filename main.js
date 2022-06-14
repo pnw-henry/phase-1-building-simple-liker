873efaf61f76a5014bddb48110bb6814fef7240c
Henry Escobar <henrye@gmail.com>
2022-06-14 15:37:06 -0700

    commit tree bd439b00c69f83d69b19125e0c37fef3988cbb6c

--- main.js
+++ main.js
@@ -1,10 +1,44 @@
-// Defining text characters for the empty and full hearts for you to use later.
-const EMPTY_HEART = '♡'
-const FULL_HEART = '♥'
+const EMPTY_HEART = '♡';
+const FULL_HEART = '♥';
+const hearts = document.querySelectorAll(".like-glyph");
+const errorMessage = document.querySelector(".hidden");
 
-// Your JavaScript code goes here!
+const likerTask = (e) => {
+  const heart = e.target;
+
+  mimicServerCall()
+  .then(function(response){
+    alert(response);
+    if (heart.innerText === "♡"){
+      heart.innerText = FULL_HEART;
+    }
+    else {
+      heart.innerText = EMPTY_HEART;
+    }
+    if (heart.className === "like-glyph"){
+      heart.className = "activated-heart";
+    }
+    else {
+      heart.className = "like-glyph";
+    }
+  })
+  .catch(function(error){
+    document.getElementById("modal-message").innerText = error;
+    if (errorMessage.classList.contains("hidden")){
+      errorMessage.classList.remove("hidden");
+    }
+    setTimeout(() => {
+      errorMessage.classList.add("hidden");
+      }, 3000);
+  })
 
 
+}
+
+
+for (const heart of hearts ){
+  heart.addEventListener("click", likerTask);
+}
 
 
 //------------------------------------------------------------------------------
